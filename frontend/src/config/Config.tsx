b6f17fdc7b7244a6fef21cfbb29ac3f2c00f6a0d
1johnwick001 <john.wick00000456@gmail.com>
2024-05-20 15:32:52 +0530

    base url

--- frontend/src/config/Config.tsx
+++ frontend/src/config/Config.tsx
@@ -1,4 +1,4 @@
-// const API_BASE_URL = "https://ludo-panel.onrender.com" 
-const API_BASE_URL = "http://localhost:8001"
+const API_BASE_URL = "https://ludo-panel.onrender.com" 
+// const API_BASE_URL = "http://localhost:8001"
 
 export default API_BASE_URL
